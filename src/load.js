18535ded9ef087d9f9338c8f4e579f05dd0ef314
Denis <nalivko.den1726@mail.ru>
2017-12-02 18:38:31 +0300

    work fine with async request

--- src/load.js
+++ src/load.js
@@ -4,8 +4,10 @@ import App from './Components/App'
 import {createStore} from "redux"
 import {reducer} from "./reducer/reducer"
 import {Provider} from "react-redux"
+import thunk from "redux-thunk"
+import {applyMiddleware} from "redux"
 
-let store = createStore(reducer);
+let store = createStore(reducer,applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store = {store}>
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,11 +1,18 @@
 /**
  * Created by Denis on 02.12.2017.
  */
-
-const initialState = {data: {}};
+import * as actions from "../constants/constants"
+const initialState = {data: {}, stage: {title: "LOADING"}};
 
 export function reducer(state = initialState,action){
+    console.log(action.type);
     switch(action.type){
+        case actions.DATA_LOADING:
+            return {...state,stage: {title: "LOADING", name: action.result.name}};
+        case actions.DATA_LOADED:
+            return {...state,data: action.result.data, stage: {title: "SUCCESS"}};
+        case actions.DATA_REJECTED:
+            return {...state,stage: {title: "ERROR",error: action.result.error,name: action.result.error}};
         default:
             return state;
     }
