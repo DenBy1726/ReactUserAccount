445d871191315bbe609c89fa66847a8bf3d61156
Denis <nalivko.den1726@mail.ru>
2017-12-02 19:11:11 +0300

    style preset and antd support

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,17 +2,17 @@
  * Created by Denis on 02.12.2017.
  */
 import * as actions from "../constants/constants"
-const initialState = {data: {}, stage: {title: "LOADING"}};
+const initialState = {data: {}, stage: {loading: true}};
 
 export function reducer(state = initialState,action){
     console.log(action.type);
     switch(action.type){
         case actions.DATA_LOADING:
-            return {...state,stage: {title: "LOADING", name: action.result.name}};
+            return {...state,stage: {loading: true, name: action.result.name}};
         case actions.DATA_LOADED:
-            return {...state,data: action.result.data, stage: {title: "SUCCESS"}};
+            return {...state,data: action.result.data, stage: {loading: false}};
         case actions.DATA_REJECTED:
-            return {...state,stage: {title: "ERROR",error: action.result.error,name: action.result.error}};
+            return {...state,stage: {loading: false,error: action.result.error,name: action.result.error}};
         default:
             return state;
     }
